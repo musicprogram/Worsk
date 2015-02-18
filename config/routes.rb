847df305123b056849d1237cd0ce33537118d349
JuanH <juan-o-henao@hotmail.es>
2015-02-17 19:59:46 -0500

    user devise sing out roots

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :boards
 
   resources :activities
@@ -15,7 +16,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+    root 'boards#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+activities = [{activityName: "correr"}]
+
+activities.each do |e|
+	Activity.create(e) unless Activity.exists?(e)
+end
+
+
+brands = [{brandName: "naiq"}]
+
+brands.each do |e|
+	Brand.create(e) unless Brand.exists?(e)
+end
+
+countries = [{countryName: "Medallo"}]
+
+countries.each do |e|
+	Country.create(e) unless Country.exists?(e)
+end
+
+employees = [{employeeName: "Juanes"}]
+
+employees.each do |e|
+	Employee.create(e) unless Employee.exists?(e)
+end
+
+campaigns = [{campaignName: "Ver arder al mundo"}]
+
+campaigns.each do |e|
+	Campaign.create(e) unless Campaign.exists?(e)
+end
