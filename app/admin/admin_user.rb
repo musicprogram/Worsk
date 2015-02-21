491bfe75ad55d08ec7d3ec7dcb0ff1468d48f49d
JuanH <juan-o-henao@hotmail.es>
2015-02-21 17:53:53 -0500

    modals-style

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register AdminUser do
   permit_params :email, :password, :password_confirmation
+    permit_params :boards, :activity,:brand,:country,:employee,:campaign
 
   index do
     selectable_column
